1f70635afac3b56c5a5841e7c415aef5e8ae475b
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-10-28 12:10:06 +0100

    Ajout choix du pokemon de base dans main.py

--- main.py
+++ main.py
@@ -31,5 +31,41 @@ while True:
     else:
         print("Nom invalide. Essaie encore !")
 
-# Message final
+
 print(f"\nBienvenue {dresseur} 👋 ! Ton aventure commence maintenant.")
+
+
+
+starters = {
+    "1": {"nom": "Flammion", "type": "Feu"},
+    "2": {"nom": "Aquabulle", "type": "Eau"},
+    "3": {"nom": "Feuillette", "type": "Plante"}
+}
+
+print("\nC’est le moment de choisir ton premier Pokémon !")
+
+while True:
+    print("1. Flammion (Feu)")
+    print("2. Aquabulle (Eau)")
+    print("3. Feuillette (Plante)")
+    choix_pokemon = input("-> ").strip().lower()  # 👈 on convertit en minuscules
+
+    # Cas 1 : le joueur tape un chiffre
+    if choix_pokemon in starters:
+        pokemon = starters[choix_pokemon]
+        break
+
+    # Cas 2 : le joueur tape un nom
+    noms_pokemons = [p["nom"].lower() for p in starters.values()]
+    if choix_pokemon in noms_pokemons:
+        for p in starters.values():
+            if p["nom"].lower() == choix_pokemon:
+                pokemon = p
+                break
+        break
+
+    print("Choix invalide. Essaie encore !")
+
+print(f"\nTu as choisi {pokemon['nom']} ({pokemon['type']})")
+
+
