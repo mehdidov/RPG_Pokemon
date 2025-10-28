e389bb089a8021d20e5d1b1662b35284d7d1f4e9
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-10-29 00:53:06 +0100

    Ajout d'un code choix de l'attaque dans le fichier combat.py

--- combat.py
+++ combat.py
@@ -1,26 +1,23 @@
-def lancer_combat(pokemon_joueur, pokemon_sauvage):
-    
+import random
 
+def lancer_combat(pokemon_joueur, pokemon_sauvage):
     print(f"\nCombat entre ton {pokemon_joueur.nom} ({pokemon_joueur.pv} PV)"
           f" et {pokemon_sauvage.nom} ({pokemon_sauvage.pv} PV) !")
 
     while pokemon_joueur.est_vivant() and pokemon_sauvage.est_vivant():
+        print("\nTon tour")
+        print("Choisis une attaque :")
+
+        for i, atq in enumerate(pokemon_joueur.attaques, 1):
+            print(f"{i}. {atq['nom']} ({atq['puissance']} dégâts)")
+
+        choix = input("-> ").strip()
+
+        # Choix de l'attaque
+        if choix.isdigit() and 1 <= int(choix) <= len(pokemon_joueur.attaques):
+            attaque_choisie = pokemon_joueur.attaques[int(choix) - 1]
+        else:
+            print("Choix invalide, une attaque aléatoire sera utilisée.")
+            attaque_choisie = random.choice(pokemon_joueur.attaques)
+
         
-        pokemon_joueur.attaquer(pokemon_sauvage)
-
-        if not pokemon_sauvage.est_vivant():
-            print(f"{pokemon_sauvage.nom} est K.O. !")
-            break
-
-    
-        pokemon_sauvage.attaquer(pokemon_joueur)
-
-        if not pokemon_joueur.est_vivant():
-            print(f"{pokemon_joueur.nom} est K.O. !")
-            break
-
-    
-    if not pokemon_joueur.est_vivant():
-        pokemon_joueur.soigner(20)
-        print(f"{pokemon_joueur.nom} reprend un peu de forces. "
-              f"({pokemon_joueur.pv}/{pokemon_joueur.pv_max} PV)")
