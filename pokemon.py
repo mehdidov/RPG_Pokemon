3a758dca2dbdab3a02fc48a8c1bbfcd05ec3f388
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-10-28 14:23:25 +0100

    Création d'une fonction est_vivant dans pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -2,7 +2,7 @@ class Pokemon:
     
     def __init__(self, nom, type_, pv, attaque):
         self.nom = nom              # Nom du Pokémon
-        self.type_ = type_          # Type : Feu / Eau / Plante
+        self.type = type          # Type : Feu / Eau / Plante
         self.pv_max = pv            # Points de vie maximum
         self.pv = pv                # Points de vie actuels
         self.attaque = attaque      # Puissance d'attaque
