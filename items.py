1880879ea396b4cc54523247ed89917990cb6c4b
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-10-29 16:50:52 +0100

    Ajout dans items.py d'une classe SuperPotion qui hérite de Item, afin de restaurer 50 pv a un pokemon, et redefinition de la methode utiliser

--- items.py
+++ items.py
@@ -16,3 +16,14 @@ class Potion(Item):
         else:
             pokemon.soigner(20)
             print(f"Tu utilises une Potion sur {pokemon.nom} !")
+
+class SuperPotion(Item):
+    def __init__(self):
+        super().__init__("Super Potion", "Restaure 50 PV à un Pokémon")
+
+    def utiliser(self, pokemon):
+        if pokemon.pv == pokemon.pv_max:
+            print(f"{pokemon.nom} a déjà tous ses PV.")
+        else:
+            pokemon.soigner(50)
+            print(f"Tu utilises une Super Potion sur {pokemon.nom} !")
