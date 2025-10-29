1d8b36f601ddcc9d59e0f3096438c368d51599fa
mehdidov <125817777+mehdidov@users.noreply.github.com>
2025-10-29 16:48:56 +0100

    Ajout dans items.py d'une classe Potion qui hérite de Item, qui afin de restaurer 20 pv a un pokemo, et redefinition de la methode utiliser

--- items.py
+++ items.py
@@ -5,3 +5,14 @@ class Item:
 
     def utiliser(self, pokemon):
         pass
+
+class Potion(Item):
+    def __init__(self):
+        super().__init__("Potion", "Restaure 20 PV à un Pokémon")
+
+    def utiliser(self, pokemon):
+        if pokemon.pv == pokemon.pv_max:
+            print(f"{pokemon.nom} a déjà tous ses PV.")
+        else:
+            pokemon.soigner(20)
+            print(f"Tu utilises une Potion sur {pokemon.nom} !")
